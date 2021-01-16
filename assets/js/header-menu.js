f4c6038ff089299c5f77bc9a1754869f807bbaed
Rafa Bruno <rafarbop@gmail.com>
2021-01-16 11:34:25 -0300

    atualizei header-menu(js) e ate desafio 21

---
+++ assets/js/header-menu.js
@@ -0,0 +1,23 @@
+var conteudoMenu = `
+
+<h1>
+    Rafa's Projects <span id="subtitulo-header">Programmer and Web Developer</span>
+</h1>
+<nav id="menu-header">
+    <a href="./index.html">Projetos Web</a>
+    <a href="./pg_python.html">Projetos Python</a>
+    <a href="">Aboutttt</a>
+    <div id="contatos">
+        <a href="">
+            <img src="/assets/img/linkedin2.png" alt="linkedIn">
+        </a>
+    <a href="">
+        <img src="/assets/img/instagram2.png" alt="Instagram">
+    </a>
+    <a href="">
+        <img src="/assets/img/mail2.png" alt="E-mail">
+    </a>
+    </div>
+</nav>`
+
+document.getElementById('header').innerHTML = conteudoMenu
