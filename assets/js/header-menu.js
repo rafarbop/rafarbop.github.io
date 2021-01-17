c31cbe93759e605e2b37a9a76952ad7e97ce4d29
Rafa Bruno <rafarbop@gmail.com>
2021-01-17 17:38:15 -0300

    Atualizado Site de Portifolio

--- assets/js/header-menu.js
+++ assets/js/header-menu.js
@@ -1,23 +1,14 @@
 var conteudoMenu = `
 
 <h1>
-    Rafa's Projects <span id="subtitulo-header">Programmer and Web Developer</span>
+    Rafael Bruno <span id="subtitulo-header">Programador e Desenvolvedor Web</span>
 </h1>
 <nav id="menu-header">
-    <a href="./index.html">Projetos Web</a>
+    <a href="/">Projetos Web</a>
     <a href="./pg_python.html">Projetos Python</a>
-    <a href="">Aboutttt</a>
-    <div id="contatos">
-        <a href="">
-            <img src="/assets/img/linkedin2.png" alt="linkedIn">
-        </a>
-    <a href="">
-        <img src="/assets/img/instagram2.png" alt="Instagram">
-    </a>
-    <a href="">
-        <img src="/assets/img/mail2.png" alt="E-mail">
-    </a>
-    </div>
+    <a href="./pg_sobre.html">Sobre</a>
+    <a href="./pg_contatos.html">Contatos</a>
+    
 </nav>`
 
 document.getElementById('header').innerHTML = conteudoMenu
