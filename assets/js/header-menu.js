ec0c2e523c95df89c548eb6e6701d24d5df09a2b
Rafa Bruno <rafarbop@gmail.com>
2021-02-09 21:19:32 -0300

    Atualizado Menu Header
    Incluido paginas separadas para os projetos python e JS, feito pagina index

--- assets/js/header-menu.js
+++ assets/js/header-menu.js
@@ -4,8 +4,9 @@ var conteudoMenu = `
     Rafael Bruno <span id="subtitulo-header">Programador e Desenvolvedor Web</span>
 </h1>
 <nav id="menu-header">
-    <a href="/">Projetos Web</a>
-    <a href="./pg_python.html">Projetos Python</a>
+    <a href="/">Início</a>
+    <a href="./pg_python.html">Python</a>
+    <a href="/pg_javascript.html">Desenvovimento Web</a>
     <a href="./pg_sobre.html">Sobre</a>
     <a href="./pg_contatos.html">Contatos</a>
     
