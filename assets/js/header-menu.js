882d0672e7ae45f567900cd42fdfdd67a773601d
Rafa Bruno <rafarbop@gmail.com>
2021-03-12 21:09:22 -0300

    Incluido alguns pesquenos testes python

--- assets/js/header-menu.js
+++ assets/js/header-menu.js
@@ -1,15 +1,11 @@
-var conteudoMenu = `
-
-<h1>
-    Rafael Bruno <span id="subtitulo-header">Programador e Desenvolvedor Web</span>
-</h1>
-<nav id="menu-header">
-    <a href="/">Início</a>
-    <a href="./pg_python.html">Python</a>
-    <a href="/pg_javascript.html">Desenvovimento Web</a>
-    <a href="./pg_sobre.html">Sobre</a>
-    <a href="./pg_contatos.html">Contatos</a>
-    
-</nav>`
-
-document.getElementById('header').innerHTML = conteudoMenu
+document.getElementById('header').innerHTML = `
+    <h1>
+        Rafael Bruno <span id="subtitulo-header">Programador e Desenvolvedor Web</span>
+    </h1>
+    <nav id="menu-header">
+        <a href="/">Início</a>
+        <a href="/pg_python.html">Python</a>
+        <a href="/pg_javascript.html">Desenvovimento Web</a>
+        <a href="/pg_sobre.html">Sobre</a>
+        <a href="/pg_contatos.html">Contatos</a>
+    </nav>`
